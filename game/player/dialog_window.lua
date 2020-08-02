e98909e517fcc113bfd59ccff6549bb6e2d27f02
sawa9889 <cfif.sawa@bk.ru>
2020-08-03 02:58:34 +0300

    Dialogs: change slide

--- game/player/dialog_window.lua
+++ game/player/dialog_window.lua
@@ -7,23 +7,25 @@ DialogWindow =
     init = function(self, displayPos, scenarioNumber)
         self.buttonToContinue = "space"
         self.scenario = require ("resource.scenario."..scenarioNumber)
-        self.currentSlide = self.scenario[1]
+        self.currentSlide = 1
         self.displayPos = displayPos
+        self.changedSlide = 0
 
         self.textHeight = 32
     end
 }
 
 function DialogWindow:update(dt)
+    self.changedSlide = self.changedSlide < 1 and self.changedSlide + dt or self.changedSlide
 
-    if love.keyboard.isDown(self.buttonToContinue) then
+    if love.keyboard.isDown(self.buttonToContinue) and self.changedSlide >= 1 then
         self:nextSlide(dt)
+        self.changedSlide = 0
     end
-    return false
+    return not self.scenario[self.currentSlide]
 end
 
 function DialogWindow:draw()
-    print('draw')
 
     love.graphics.setColor(255, 255, 0)
     love.graphics.rectangle( 'fill', self.displayPos.x,      self.displayPos.y,       16, 64 - self.textHeight )
@@ -32,14 +34,21 @@ function DialogWindow:draw()
     love.graphics.setColor(0, 0, 0)
     love.graphics.rectangle( 'fill', self.displayPos.x,      self.displayPos.y + 32,  64, self.textHeight )
     love.graphics.setColor(1, 1, 1)
-    love.graphics.printf(self.currentSlide.whoTalks, Fonts.small, self.displayPos.x + 16 + (32 - self.currentSlide.whoTalks:len()*4)/2, self.displayPos.y + (64 - self.textHeight - 4), 16)
-    love.graphics.printf(self.currentSlide.text, Fonts.medium, self.displayPos.x, self.displayPos.y + (64 - self.textHeight), 64)
-
+    love.graphics.printf(self.scenario[self.currentSlide].whoTalks, 
+                         Fonts.small, 
+                         self.displayPos.x + 16 + (32 - self.scenario[self.currentSlide].whoTalks:len()*4)/2, 
+                         self.displayPos.y + (64 - self.textHeight - 4), 
+                         16)
+    love.graphics.printf(self.scenario[self.currentSlide].text, 
+                         Fonts.medium, 
+                         self.displayPos.x, 
+                         self.displayPos.y + (64 - self.textHeight), 
+                         64)
 
 end
 
 function DialogWindow:nextSlide()
-
+    self.currentSlide = self.currentSlide + 1
 end
 
 return DialogWindow
