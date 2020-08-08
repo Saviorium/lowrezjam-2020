bb0bb54cbe4b0361ff48d22a532cf7697d939c38
savior <saviorium@gmail.com>
2020-08-08 14:56:09 +0300

    Housekeeping, refactoring

--- game/debug.lua
+++ game/debug.lua
@@ -1,7 +1,7 @@
 Debug = {
-    DrawDebugForPlayer = 1,
+    DrawDebugForPlayer = 0,
     DrawDebugForMapPiece = 0,
-    DrawDebugColliders = 1
+    DrawDebugColliders = 0
 }
 
 -- usage: vardump(x1, test, myVar) or vardump({ship = self, dt = dt})
