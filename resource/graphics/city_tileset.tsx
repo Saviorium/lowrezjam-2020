1cdda5787363556a72b413b513b870f955ba6234
savior <saviorium@gmail.com>
2020-08-10 02:56:20 +0300

    Fix grid size

--- resource/graphics/city_tileset.tsx
+++ resource/graphics/city_tileset.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="city_tileset" tilewidth="8" tileheight="8" tilecount="64" columns="8">
+<tileset version="1.4" tiledversion="1.4.2" name="city_tileset" tilewidth="4" tileheight="4" tilecount="256" columns="16">
+ <editorsettings>
+  <export target="../maps/city_tileset..tsx" format="tsx"/>
+ </editorsettings>
  <image source="city_tileset.png" width="64" height="64"/>
 </tileset>
