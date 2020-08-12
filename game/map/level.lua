4cdc3866d90f8f63baebc886f2fe6cc189d59ec5
sawa9889 <cfif.sawa@bk.ru>
2020-08-13 01:08:42 +0300

    Added second map

--- game/map/level.lua
+++ game/map/level.lua
@@ -132,6 +132,7 @@ end
 
 LEVELS = {
     first_level = {a = {1, 1, 1}},
+    second_level = {a = {1, 1, 1}},
     sample_level = {r = {1, 0, 0}, g = {0, 1, 0}, b = {0, 0, 1}},
     rainbow_test = {
         a = {1,0,0},
